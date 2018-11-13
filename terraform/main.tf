70d06afcf09b9d4088fd2ed8d4900d153f4f2b22
Ivan Kosolapov <ikosolapov@express42.com>
2018-11-13 18:00:29 +0300

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,151 @@
+provider "google" {
+  project = "openshift-playground-222412"
+  region  = "europe-west4"
+}
+
+resource "google_compute_network" "openshift" {
+  name = "openshift"
+}
+
+resource "google_compute_instance" "master" {
+  name = "openshift-master"
+
+  boot_disk {
+    initialize_params {
+      image = "${var.base_image}"
+      size  = "${var.openshift_master_disk_size}"
+    }
+  }
+
+  network_interface {
+    network = "${google_compute_network.openshift.name}"
+  }
+
+  tags = [
+    "openshift-master",
+  ]
+}
+
+resource "google_compute_instance" "nodes" {
+  count = "${var.openshift_nodes_count}"
+  name  = "openshift-node-${count.index + 1}"
+
+  boot_disk {
+    initialize_params {
+      image = "${var.base_image}"
+      size  = "${var.openshift_nodes_disk_size}"
+    }
+  }
+
+  network_interface {
+    network = "${google_compute_network.openshift.name}"
+  }
+
+  tags = [
+    "openshift-node",
+  ]
+}
+
+resource "google_compute_instance" "ansible_controller" {
+  name = "ansible-controller"
+
+  boot_disk {
+    initialize_params {
+      image = "${var.base_image}"
+    }
+  }
+
+  network_interface {
+    network = "${google_compute_network.openshift.name}"
+  }
+
+  tags = [
+    "ansible-controller",
+  ]
+}
+
+resource "google_compute_firewall" "openshift_node_to_node" {
+  name    = "openshift_node_to_node"
+  network = "${google_compute_network.openshift.name}"
+
+  # Required for SDN communication between pods on separate hosts.
+  allow {
+    protocol = "udp"
+    ports    = ["4789"]
+  }
+
+  source_tags = ["openshift-node"]
+  target_tags = ["openshift-node"]
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "openshift_node_to_master" {
+  name    = "openshift-node-to-master"
+  network = "${google_compute_network.openshift.name}"
+
+  # Required for SDN communication between pods on separate hosts.
+  allow = {
+    protocol = "udp"
+    ports    = ["4789"]
+  }
+
+  # Required for node hosts to communicate to the master API, for the node hosts to post back status, to receive tasks, and so on.
+  allow = {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  # Required for DNS resolution of cluster services (SkyDNS).
+  allow {
+    ports = ["8053"]
+  }
+
+  source_tags = ["openshift-node"]
+  target_tags = ["openshift-master"]
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "openshift_master_to_node" {
+  name    = "openshift-master-to-node"
+  network = "${google_compute_network.openshift.name}"
+
+  # Required for SDN communication between pods on separate hosts.
+  allow = {
+    protocol = "udp"
+    ports    = ["4789"]
+  }
+
+  # The master proxies to node hosts via the Kubelet for oc commands.
+  allow = {
+    protocol = "tcp"
+    ports    = ["10250"]
+  }
+
+  source_tags = ["openshift-master"]
+  target_tags = ["openshift-node"]
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "openshift_external_to_master" {
+  name    = "openshift-external-to-master"
+  network = "${google_compute_network.openshift.name}"
+
+  # Required for node hosts to communicate to the master API, for node hosts to post back status, to receive tasks, and so on.
+  allow = {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  # Port that the controller service listens on. Required to be open for the /metrics and /healthz endpoints.
+  allow {
+    protocol = "tcp"
+    ports    = ["8444"]
+  }
+
+  target_tags = ["openshift-master"]
+
+  source_ranges = ["0.0.0.0/0"]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "base_image" {
+  description = "Base VM image for Openshift Nodes"
+  default     = "rhel-7-v20181011"
+}
+
+variable "openshift_master_machine_type" {
+  default = "custom-6-20480"
+}
+
+variable "openshift_node_machine_type" {
+  default = "custom-1-10240"
+}
+
+variable "openshift_nodes_count" {
+  default = 2
+}
+
+variable "openshift_nodes_disk_size" {
+  size = 40
+}
+
+variable "openshift_master_disk_size" {
+  size = 60
+}
