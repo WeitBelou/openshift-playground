766cbb2bcdb70fb2473cf6acb1752f5460fc0ed6
Ivan Kosolapov <ikosolapov@express42.com>
2018-11-22 15:53:38 +0300

    Open ports from openshift to ansible controller

--- terraform/network.tf
+++ terraform/network.tf
@@ -32,7 +32,6 @@ resource "google_compute_firewall" "external_to_ansible_controller" {
   name    = "external-to-ansible-controller"
   network = "${google_compute_network.openshift.name}"
 
-  # External SSH access to controller
   allow {
     protocol = "tcp"
     ports    = ["22"]
@@ -43,11 +42,26 @@ resource "google_compute_firewall" "external_to_ansible_controller" {
   source_ranges = ["${var.trusted_ip_ranges}"]
 }
 
+resource "google_compute_firewall" "ansible_controller_to_external_egress" {
+  name    = "ansible-controller-to-external-egress"
+  network = "${google_compute_network.openshift.name}"
+
+  direction = "EGRESS"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
+  target_tags = ["ansible-controller"]
+
+  destination_ranges = ["0.0.0.0/0"]
+}
+
 resource "google_compute_firewall" "ansible_controller_to_openshift" {
   name    = "ansible-controller-to-openshift"
   network = "${google_compute_network.openshift.name}"
 
-  # External SSH access to controller
   allow {
     protocol = "tcp"
     ports    = ["22"]
@@ -55,6 +69,21 @@ resource "google_compute_firewall" "ansible_controller_to_openshift" {
 
   source_tags = ["ansible-controller"]
   target_tags = ["openshift-master", "openshift-node"]
+
+  source_ranges = []
+}
+
+resource "google_compute_firewall" "http_https_openshift_to_ansible_controller" {
+  name    = "http-https-openshift-to-ansible-controller"
+  network = "${google_compute_network.openshift.name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
+  target_tags = ["ansible-controller"]
+  source_tags = ["openshift-master", "openshift-node"]
 
   source_ranges = []
 }
