abb8b3df80d7956483ca24aba6238e88ce0b3bab
Ivan Kosolapov <ikosolapov@express42.com>
2018-11-23 11:30:58 +0300

    Try to use google-cloud managed dns

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -1,11 +1,19 @@
+data "google_project" "project" {}
+
 resource "template_dir" "ansible" {
   source_dir      = "../ansible"
   destination_dir = "/tmp/ansible"
 
   vars {
-    openshift_master_internal_ip   = "${google_compute_instance.openshift_master.network_interface.0.network_ip}"
-    openshift_node_1_internal_ip   = "${google_compute_instance.openshift_nodes.0.network_interface.0.network_ip}"
-    openshift_node_2_internal_ip   = "${google_compute_instance.openshift_nodes.1.network_interface.0.network_ip}"
+    openshift_master_internal_ip = "${google_compute_instance.openshift_master.network_interface.0.network_ip}"
+    openshift_master_fqdn        = "${google_compute_instance.openshift_master.name}.${google_compute_instance.openshift_master.zone}.${data.google_project.project.name}.internal"
+
+    openshift_node_1_internal_ip = "${google_compute_instance.openshift_nodes.0.network_interface.0.network_ip}"
+    openshift_node_1_fqdn        = "$${google_compute_instance.openshift_nodes.0.name}.${google_compute_instance.openshift_nodes.0.zone}.${data.google_project.project.name}.internal"
+
+    openshift_node_2_internal_ip = "${google_compute_instance.openshift_nodes.1.network_interface.0.network_ip}"
+    openshift_node_2_fqdn        = "$${google_compute_instance.openshift_nodes.1.name}.${google_compute_instance.openshift_nodes.1.zone}.${data.google_project.project.name}.internal"
+
     ansible_controller_internal_ip = "${google_compute_instance.ansible_controller.network_interface.0.network_ip}"
   }
 }
